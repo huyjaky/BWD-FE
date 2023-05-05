2164440357af86946b1776d0f3dd00466d4cf15a
huyjaky <huyphanuyg@gmail.com>
2023-05-05 17:50:14 +0700

    calender

--- src/components/headers/headerMain/controlPlan/controlBar/popOverDetail/checkIn.tsx
+++ src/components/headers/headerMain/controlPlan/controlBar/popOverDetail/checkIn.tsx
@@ -1,11 +1,90 @@
-
+import {
+  add,
+  eachDayOfInterval,
+  endOfMonth,
+  endOfWeek,
+  format,
+  getDay,
+  isSameMonth,
+  isToday,
+  parse,
+  startOfToday,
+  startOfWeek
+} from 'date-fns';
+import { useState } from 'react';
 const CheckIn = () => {
+  const today = startOfToday();
+  const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+  const colStartClasses = [
+    '',
+    'col-start-2',
+    'col-start-3',
+    'col-start-4',
+    'col-start-5',
+    'col-start-6',
+    'col-start-7'
+  ];
 
+  const [currMonth, setCurrMonth] = useState(() => format(today, 'MMM-yyyy'));
+  let firstDayOfMonth = parse(currMonth, 'MMM-yyyy', new Date());
+
+  const daysInMonth = eachDayOfInterval({
+    start: startOfWeek(firstDayOfMonth),
+    end: endOfWeek(endOfMonth(firstDayOfMonth))
+  });
+
+  const getPrevMonth = (event: React.MouseEvent<SVGSVGElement>) => {
+    event.preventDefault();
+    const firstDayOfPrevMonth = add(firstDayOfMonth, { months: -1 });
+    setCurrMonth(format(firstDayOfPrevMonth, 'MMM-yyyy'));
+  };
+
+  const getNextMonth = (event: React.MouseEvent<SVGSVGElement>) => {
+    event.preventDefault();
+    const firstDayOfNextMonth = add(firstDayOfMonth, { months: 1 });
+    setCurrMonth(format(firstDayOfNextMonth, 'MMM-yyyy'));
+  };
   return (
     <div className="w-full h-full bg-white rounded-2xl pointer-events-auto" id="checkin-popup">
-
+      <div className="p-8 w-full h-full flex items-center justify-center">
+        <div className="w-full h-full">
+          <div className="flex items-center justify-between">
+            <p className="font-semibold text-xl">{format(firstDayOfMonth, 'MMMM yyyy')}</p>
+            <div className="flex items-center justify-evenly gap-6 sm:gap-10">
+              Left
+              Right
+            </div>
+          </div>
+          <hr className="my-6" />
+          <div className="grid grid-cols-7 gap-6 sm:gap-10 place-items-center">
+            {days.map((day, idx) => {
+              return (
+                <div key={idx} className="font-semibold">
+                  {day}
+                </div>
+              );
+            })}
+          </div>
+          <div className="grid grid-cols-7 gap-6 sm:gap-10 mt-8 place-items-center">
+            {daysInMonth.map((day, idx) => {
+              return (
+                <div key={idx} className={colStartClasses[getDay(day)]}>
+                  <p
+                    className={`cursor-pointer flex items-center justify-center font-semibold h-8 w-8 rounded-full  hover:text-white ${
+                      isSameMonth(day, today) ? 'text-gray-900' : 'text-gray-400'
+                    } ${!isToday(day) && 'hover:bg-blue-500'} ${
+                      isToday(day) && 'bg-red-500 text-white'
+                    }`}>
+                    {format(day, 'd')}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CheckIn;
