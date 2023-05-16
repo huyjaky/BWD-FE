6047ff4cae42603133a7c0dc4ae83285b3acc28a
huyjaky <huyphanuyg@gmail.com>
2023-05-16 18:42:36 +0700

    hoan thien co ban show house

--- src/components/main/showHouse/carousel.tsx
+++ src/components/main/showHouse/carousel.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { wrap } from "popmotion";
+import { AnimatePresence, Variants, motion, useMotionValue, useTransform } from 'framer-motion';
+import { wrap } from 'popmotion';
+import { useEffect, useState } from 'react';
+import { GrCaretNext, GrCaretPrevious } from 'react-icons/gr';
 
 const variants = {
   enter: (direction: number) => {
@@ -24,72 +24,107 @@ const variants = {
   }
 };
 
-/**
- * Experimenting with distilling swipe offset and velocity into a single variable, so the
- * less distance a user has swiped, the more velocity they need to register as a swipe.
- * Should accomodate longer swipes and short flicks without having binary checks on
- * just distance thresholds and velocity > 0.
- */
+const variantsBtn: Variants = {
+  hiddenLeft: {
+    left: -100
+  },
+  showLeft: {
+    left: 8
+  },
+
+  hiddenRight: {
+    right: 8
+  },
+  showRight: {
+    right: -100
+  }
+};
+
 const swipeConfidenceThreshold = 10000;
 const swipePower = (offset: number, velocity: number) => {
   return Math.abs(offset) * velocity;
 };
 
 interface CarouselProps {
-  arrImg:{Path: string}[]
+  arrImg: { Path: string }[];
 }
 
-const Carousel = ({arrImg}:CarouselProps ) => {
-  const [[page, direction], setPage] = useState([0, 0]);
+const styleBtn =
+  'absolute top-[calc(50%-20px)] z-10 bg-white rounded-full w-10 h-10 flex items-center justify-center';
 
-  // We only have 3 images, but we paginate them absolutely (ie 1, 2, 3, 4, 5...) and
-  // then wrap that within 0-2 to find our image ID in the array below. By passing an
-  // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
-  // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
+const Carousel = ({ arrImg }: CarouselProps) => {
+  const [[page, direction], setPage] = useState([0, 0]);
+  const [isHover, setIsHover] = useState(false);
+
   const imageIndex = wrap(0, arrImg.length, page);
 
   const paginate = (newDirection: number) => {
     setPage([page + newDirection, newDirection]);
   };
 
+  const handleOnEnter = (event: any) => {
+    setIsHover(true);
+  };
+
+  const handleOnLeave = (event: any) => {
+    setIsHover(false);
+  };
+
   return (
     <>
-      <AnimatePresence initial={false} custom={direction}>
-        <motion.img
-          key={page}
-          src={arrImg[imageIndex].Path}
-          custom={direction}
-          variants={variants}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          transition={{
-            x: { type: "spring", stiffness: 300, damping: 30 },
-            opacity: { duration: 0.2 }
-          }}
-          drag="x"
-          dragConstraints={{ left: 0, right: 0 }}
-          dragElastic={1}
-          onDragEnd={(e, { offset, velocity }) => {
-            const swipe = swipePower(offset.x, velocity.x);
+      <motion.div
+        className="w-full h-full relative flex overflow-hidden rounded-xl"
+        onHoverStart={handleOnEnter}
+        onHoverEnd={handleOnLeave}>
+        <AnimatePresence initial={false} custom={direction}>
+          <motion.img
+            key={page}
+            src={arrImg[imageIndex].Path}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            className="w-full h-full absolute object-cover"
+            transition={{
+              x: { type: 'spring', stiffness: 200, damping: 30 },
+              opacity: { duration: 0.5 }
+            }}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x);
 
-            if (swipe < -swipeConfidenceThreshold) {
-              paginate(1);
-            } else if (swipe > swipeConfidenceThreshold) {
-              paginate(-1);
-            }
-          }}
-        />
-      </AnimatePresence>
-      <div className="next" onClick={() => paginate(1)}>
-        {"‣"}
-      </div>
-      <div className="prev" onClick={() => paginate(-1)}>
-        {"‣"}
-      </div>
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1);
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1);
+              }
+            }}
+          />
+        </AnimatePresence>
+        <motion.button
+          variants={variantsBtn}
+          onClick={() => paginate(-1)}
+          initial={isHover ? 'hiddenLeft' : 'showLeft'}
+          animate={isHover ? 'showLeft' : 'hiddenLeft'}
+          transition={{ duration: 0.5, type: 'tween' }}
+          className={`${styleBtn} left-[10px]`}>
+          <GrCaretPrevious />
+        </motion.button>
+        <motion.button
+          variants={variantsBtn}
+          onClick={() => paginate(1)}
+          initial={isHover ? 'showRight' : 'hiddenRight'}
+          animate={isHover ? 'hiddenRight' : 'showRight'}
+          transition={{ duration: 0.5, type: 'tween' }}
+          className={`${styleBtn} right-[10px]`}>
+          <GrCaretNext />
+        </motion.button>
+      </motion.div>
     </>
   );
 };
-
 
 export default Carousel;
--- src/components/main/showHouse/showHouse.tsx
+++ src/components/main/showHouse/showHouse.tsx
@@ -9,16 +9,29 @@ const ShowHouse = () => {
   return (
     <div>
       <motion.div className="w-full h-fit">
-        <motion.div className='w-full grid grid-cols-houseBox h-fit'>
+        <motion.div className="w-full grid grid-cols-houseBox h-fit">
           {house.map((item: house_, index: number) => {
-            return <motion.div key={index} className='w-[300px] h-[400px]'>
-              <div className='w-full h-[400px] bg-slate-500'>
-                <Carousel arrImg={item.arrImg}/>
-              </div>
-              <div className='h-auto w-full bg-emerald-300'>
-
-              </div>
-            </motion.div>;
+            return (
+              <motion.div key={index} className="w-[300px] h-[400px] ">
+                <div className="w-full h-[300px]">
+                  <Carousel arrImg={item.arrImg} />
+                </div>
+                <div className="h-[100px] w-full box-border p-2">
+                  <div className="w-full h-fit flex font-semibold">
+                    <div className="flex-[2]">
+                      <span>
+                        {item.address.adminDistrict2}, {item.address.countryRegion}
+                      </span>
+                    </div>
+                    <div className="flex-1 flex justify-end">star</div>
+                  </div>
+                  <div className="w-full h-fit mt-1">{item.useracc.UserName}</div>
+                  <div className="w-full h-fit mt-1 ">
+                    <span className='font-semibold'>&#36;{item.Price}</span> night
+                  </div>
+                </div>
+              </motion.div>
+            );
           })}
         </motion.div>
       </motion.div>
