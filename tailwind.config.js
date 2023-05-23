a6721ef59d299ca6b0328248289f78daabe4a670
huyjaky <huyphanuyg@gmail.com>
2023-05-23 19:56:01 +0700

    daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -167,6 +167,7 @@ module.exports = {
   },
   plugins: [
     require('tailwind-scrollbar-hide'),
-    require('@savvywombat/tailwindcss-grid-areas')
+    require('@savvywombat/tailwindcss-grid-areas'),
+    require("daisyui")
   ]
 };
